d0ed5554a6204f368b30a28be60e49d9693525d9
AjayGitHubCraft <145157684+ajaysharma0@users.noreply.github.com>
2024-03-03 01:14:56 +0530

    Create script.js

---
+++ Lec3/script.js
@@ -0,0 +1,10 @@
+for(let i=1;i<=5;i++){
+     console.log("Sita Ram");
+}
+
+
+let value = { prop1: 'value1', prop2: 'value2', prop3: 'value3' };
+
+for (let key in value) {
+    console.log( key, value[key]);
+}
