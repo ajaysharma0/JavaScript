1817ec382c333e676a78350458f704a0a9543713
AjayGitHubCraft <145157684+ajaysharma0@users.noreply.github.com>
2024-03-16 03:21:30 +0530

    Create Script.js

---
+++ Lec5/Script.js
@@ -0,0 +1,26 @@
+let str="apna College";
+str.method();
+
+//Trim Methods
+let msg = "  hello  ";
+console.log(msg.trim());
+
+let password=prompt("set your password");
+let newpass=password.trim();
+console.log(newpass);
+
+//toLowerCase & toU
+let nam="APNA COLLEGE";
+//console.log(nam.toLowerCase()); This is printing statement inside console.log();
+let lowercase=nam.toLowerCase();
+console.log(lowercase);
+
+let str="holy river";
+let res=str.toUpperCase();
+console.log(res);
+
+
+
+
+
+
